a650a2e09da4a1d23fc7416ebd69bcaecfe132fb
leonardo oteca <leonardo@sinchicreaciones.com>
2019-08-27 08:30:47 -0500

    gitignore updated

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { render } from 'react-dom'
 
 import { App } from './app'
 
+
+//the Suspense Component has a fallback prop that receives a component like a loading message while translations load
+
 render(< Suspense fallback='loading...'>
     <App />
     </Suspense>
